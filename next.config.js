b18e6efb24f1a2ca7aad79448878b279dd1b8d7e
moss <moss.piglets.site@gmail.com>
2023-01-17 12:50:18 +0800

    fix: re-add next config

--- next.config.js
+++ next.config.js
@@ -1,93 +1,103 @@
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// })
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
-// const withPWA = require('next-pwa')({
-//   dest: 'public',
-//   disable: process.env.NODE_ENV === 'development',
-// })
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})
 
-// const nextConfig = {
-//   webpack(config, { isServer }) {
-//     // audio support
-//     config.module.rules.push({
-//       test: /\.(ogg|mp3|wav|mpe?g)$/i,
-//       exclude: config.exclude,
-//       use: [
-//         {
-//           loader: require.resolve('url-loader'),
-//           options: {
-//             limit: config.inlineImageLimit,
-//             fallback: require.resolve('file-loader'),
-//             publicPath: `${config.assetPrefix}/_next/static/images/`,
-//             outputPath: `${isServer ? '../' : ''}static/images/`,
-//             name: '[name]-[hash].[ext]',
-//             esModule: config.esModule || false,
-//           },
-//         },
-//       ],
-//     })
+const nextConfig = {
+  webpack(config, { isServer }) {
+    // audio support
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      exclude: config.exclude,
+      use: [
+        {
+          loader: require.resolve('url-loader'),
+          options: {
+            limit: config.inlineImageLimit,
+            fallback: require.resolve('file-loader'),
+            publicPath: `${config.assetPrefix}/_next/static/images/`,
+            outputPath: `${isServer ? '../' : ''}static/images/`,
+            name: '[name]-[hash].[ext]',
+            esModule: config.esModule || false,
+          },
+        },
+      ],
+    })
 
-//     // shader support
-//     config.module.rules.push({
-//       test: /\.(glsl|vs|fs|vert|frag)$/,
-//       exclude: /node_modules/,
-//       use: ['raw-loader', 'glslify-loader'],
-//     })
+    // shader support
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    })
 
-//     return config
-//   },
+    return config
+  },
 
-// }
-
-// nextConfig.images = {
-//   domains: ['landhills.co', 'landhills.netlify.app'],
-// }
-
-// // manage i18n
-// // if (process.env.EXPORT !== 'true') {
-// //   nextConfig.i18n = {
-// //     locales: ['en', 'jp'],
-// //     defaultLocale: 'en',
-// //   }
-// // }
-
-// const KEYS_TO_OMIT = [
-//   'webpackDevMiddleware',
-//   'configOrigin',
-//   'target',
-//   'analyticsId',
-//   'webpack5',
-//   'amp',
-//   'assetPrefix',
-//   'experimental',
-// ]
-
-// module.exports = (_phase, { defaultConfig }) => {
-//   const plugins = [[withPWA], [withBundleAnalyzer, {}]]
-
-//   const wConfig = plugins.reduce(
-//     (acc, [plugin, config]) => plugin({ ...acc, ...config }),
-//     {
-//       ...defaultConfig,
-//       ...nextConfig,
-//     }
-//   )
-
-//   const finalConfig = {}
-//   Object.keys(wConfig).forEach((key) => {
-//     if (!KEYS_TO_OMIT.includes(key)) {
-//       finalConfig[key] = wConfig[key]
-//     }
-//   })
-
-//   return finalConfig
-// }
-
-
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['landhills.co', 'landhills.netlify.app'],
-  }
 }
+
+nextConfig.images = {
+  domains: ['landhills.co', 'landhills.netlify.app'],
+}
+nextConfig.env = {
+  NEXT_PUBLIC_API_URL: 'https://landhills.co/wp-json/wp/v2',
+  NEXT_PUBLIC_SITE_URL: 'https://landhills.netlify.app'
+}
+
+
+
+// manage i18n
+// if (process.env.EXPORT !== 'true') {
+//   nextConfig.i18n = {
+//     locales: ['en', 'jp'],
+//     defaultLocale: 'en',
+//   }
+// }
+
+const KEYS_TO_OMIT = [
+  'webpackDevMiddleware',
+  'configOrigin',
+  'target',
+  'analyticsId',
+  'webpack5',
+  'amp',
+  'assetPrefix',
+  'experimental',
+]
+
+module.exports = (_phase, { defaultConfig }) => {
+  const plugins = [[withPWA], [withBundleAnalyzer, {}]]
+
+  const wConfig = plugins.reduce(
+    (acc, [plugin, config]) => plugin({ ...acc, ...config }),
+    {
+      ...defaultConfig,
+      ...nextConfig,
+    }
+  )
+
+  const finalConfig = {}
+  Object.keys(wConfig).forEach((key) => {
+    if (!KEYS_TO_OMIT.includes(key)) {
+      finalConfig[key] = wConfig[key]
+    }
+  })
+
+  return finalConfig
+}
+
+
+// module.exports = {
+//   swcMinify: true,
+//   env: {
+//     NEXT_PUBLIC_API_URL: 'https://landhills.co/wp-json/wp/v2',
+//     NEXT_PUBLIC_SITE_URL: 'https://landhills.netlify.app'
+//   },
+//   images: {
+//     domains: ['landhills.co', 'landhills.netlify.app'],
+//   }
+// }
