62081e91cadcbdd172c44888586e0e652e008f7a
moss <moss.piglets.site@gmail.com>
2023-05-02 09:46:49 +0800

    fix: empty project state

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,28 +7,28 @@ export default function Projects({ firstpageProjectPool, newComingIn }) {
     return (
         <div style={{ minHeight: '100vh' }}>
             {
-                firstpageProjectPool.length &&
-                firstpageProjectPool?.map((project, i) => (
-                    <StyledItems key={i}
-                        className='projects-wrap'
-                    >
-                        <Link href={`${process.env.NODE_ENV !== 'production'
-                            ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_SITE_URL}/projects/${project.id}`}>
-                            <StyledImage
-                                className="images"
-                                id={project.id}
-                                draggable="false"
-                                src={project.image}
-                                alt="image"
-                                style={i % 2 === 0 ? { maxWidth: '600px' } : { maxHeight: '900px' }}
-                            />
-                            <StyledImageInfo className='projects' >
-                                <p > {project.title} </p>
-                                <p> {project.no} </p>
-                            </StyledImageInfo>
-                        </Link>
-                    </StyledItems>
-                ))
+                firstpageProjectPool.length ?
+                    firstpageProjectPool?.map((project, i) => (
+                        <StyledItems key={i}
+                            className='projects-wrap'
+                        >
+                            <Link href={`${process.env.NODE_ENV !== 'production'
+                                ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_SITE_URL}/projects/${project.id}`}>
+                                <StyledImage
+                                    className="images"
+                                    id={project.id}
+                                    draggable="false"
+                                    src={project.image}
+                                    alt="image"
+                                    style={i % 2 === 0 ? { maxWidth: '600px' } : { maxHeight: '900px' }}
+                                />
+                                <StyledImageInfo className='projects' >
+                                    <p > {project.title} </p>
+                                    <p> {project.no} </p>
+                                </StyledImageInfo>
+                            </Link>
+                        </StyledItems>
+                    )) : ''
             }
             {
                 newComingIn?.map((project, i) => (
